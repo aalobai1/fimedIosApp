4b29bf93257fdf64892fa3cdac0394b8d3e4443d
Ali Alobaidi <alinawar2000@gmail.com>
2019-10-05 12:50:47 -0400

    Initial Commit

---
+++ aToastie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aToastie
+//
+//  Created by Ali Alobaidi on 10/5/19.
+//  Copyright © 2019 Ali Alobaidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
